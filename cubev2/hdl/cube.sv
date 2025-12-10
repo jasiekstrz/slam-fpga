27a9ab43c6b9e95610adcb521afbc54794aae48b
Jasiek Strzeszynski <jasiekstrzeszynski@gmail.com>
2025-12-10 13:19:03 -0500

    cubev2 packed cube_pts

--- cubev2/hdl/cube.sv
+++ cubev2/hdl/cube.sv
@@ -1,24 +1,17 @@
 module cube #
     (
         parameter integer C_S00_AXIS_TDATA_WIDTH    = 32,
-        parameter integer C_S01_AXIS_TDATA_WIDTH    = 32,
         parameter integer C_M00_AXIS_TDATA_WIDTH    = 32
     )
     (
+        input wire [7:0][31:0] cube_pts,
  
-        // Ports of Axi Slave Bus Interface S00_AXIS  -  Cube Points
+        // Ports of Axi Slave Bus Interface S00_AXIS  -  Frame Data
         input wire  s00_axis_aclk, s00_axis_aresetn,
         input wire  s00_axis_tlast, s00_axis_tvalid,
         input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
         input wire [(C_S00_AXIS_TDATA_WIDTH/8)-1: 0] s00_axis_tstrb,
         output logic  s00_axis_tready,
- 
-        // Ports of Axi Slave Bus Interface S01_AXIS  -  Frame Data
-        input wire  s01_axis_aclk, s01_axis_aresetn,
-        input wire  s01_axis_tlast, s01_axis_tvalid,
-        input wire [C_S01_AXIS_TDATA_WIDTH-1 : 0] s01_axis_tdata,
-        input wire [(C_S01_AXIS_TDATA_WIDTH/8)-1: 0] s01_axis_tstrb,
-        output logic  s01_axis_tready,
  
         // Ports of Axi Master Bus Interface M00_AXIS  -  Modified Frame
         input wire  m00_axis_aclk, m00_axis_aresetn,
@@ -36,17 +29,22 @@ module cube #
     
 
 
-    logic [2:0] cur_cube_point;
     logic [CORD_SIZE-1:0] cube_x [7:0];
     logic [CORD_SIZE-1:0] cube_y [7:0];
+
+    always_comb begin
+        for (int i = 0; i < 8; i = i + 1) begin
+            cube_x[i] = cube_pts[i][CORD_SIZE-1:0];
+            cube_y[i] = cube_pts[i][16+CORD_SIZE-1:16];
+        end
+    end
 
     logic [CORD_SIZE-1:0] cur_x;
     logic [CORD_SIZE-1:0] cur_y;
 
 
 
-    assign s00_axis_tready = 1;
-    assign s01_axis_tready = m00_axis_tready;
+    assign s00_axis_tready = m00_axis_tready;
     assign m00_axis_tstrb  = {C_M00_AXIS_TDATA_WIDTH/8{1'b1}};
 
 
@@ -79,14 +77,7 @@ module cube #
             cur_y <= 0;
             m00_axis_tvalid <= 0;
         end else begin
-            if (s00_axis_tvalid) begin
-                cube_x[cur_cube_point] <= s00_axis_tdata[CORD_SIZE-1:0];
-                cube_y[cur_cube_point] <= s00_axis_tdata[2*CORD_SIZE-1:CORD_SIZE];
-
-                cur_cube_point <= cur_cube_point + 1;
-            end
-
-            if (s01_axis_tready && s01_axis_tvalid) begin
+            if (s00_axis_tready && s00_axis_tvalid) begin
                 if (is_on_segment(cur_x, cur_y, cube_x[0], cube_y[0], cube_x[1], cube_y[1]) ||
                     is_on_segment(cur_x, cur_y, cube_x[2], cube_y[2], cube_x[3], cube_y[3]) ||
                     is_on_segment(cur_x, cur_y, cube_x[4], cube_y[4], cube_x[5], cube_y[5]) ||
@@ -104,11 +95,11 @@ module cube #
                 ) begin  
                     m00_axis_tdata <= {8'hFF, 8'h00, 8'h00};
                 end else begin
-                    m00_axis_tdata <= s01_axis_tdata;
+                    m00_axis_tdata <= s00_axis_tdata;
                 end
 
                 m00_axis_tvalid <= 1;
-                m00_axis_tlast <= s01_axis_tlast;
+                m00_axis_tlast <= s00_axis_tlast;
 
                 if (cur_x == MAX_X-1) begin
                     if (cur_y == MAX_Y-1) begin
